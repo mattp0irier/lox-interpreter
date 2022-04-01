ca44c6cdb94857e12084c38ff616bc4af84b60c0
tw-russell <72945723+tw-russell@users.noreply.github.com>
2022-04-01 01:43:07 -0500

    Enter a message <Required>

--- trmpLox/trmpLox/runtimeError.cs
+++ trmpLox/trmpLox/runtimeError.cs
@@ -6,10 +6,14 @@ using System.Threading.Tasks;
 
 namespace trmpLox
 {
+    //runtimeError: runtimeErrors are unique exceptions that are thrown
+    //when an invalid operation is attempted
     public class runtimeError : Exception
     {
+        // keeps track of the problematic token
         public readonly Token token;
 
+        // constructor includes Exception(string) constructor
         public runtimeError(Token token, string msg) : base(msg)
         {
             this.token = token;
